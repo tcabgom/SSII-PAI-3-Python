f26ef8b4df712eb984d8e678d09592e32a5e10b6
tcabgom <tcabgom222@gmail.com>
2024-04-04 19:57:44 +0200

    feat: Added logs again

--- BYODServer.py
+++ BYODServer.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import socket
 import threading
 from OpenSSL import SSL
@@ -12,9 +13,11 @@ PORT = 7070
 KEYFILE = 'server-key.pem'
 CERTFILE = 'server-cert.pem'
 
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def handle_client(connection_socket, address):
     try:
-        print("Conexión aceptada desde:", address)
+        logging.info("Conexión aceptada desde: %s", address)
 
         data = connection_socket.recv(1024).decode()
         client_data = json.loads(data)
@@ -25,18 +28,19 @@ def handle_client(connection_socket, address):
         # Autenticar al usuario
         auth_result = authenticate_user(connection_socket, username, password)
         if not auth_result:
-            print("Autenticación fallida."+str(address))
+            logging.warning("Autenticación fallida de %s", address)
             return
         
-        print("Mensaje recibido del cliente:", message)
+        logging.info("Mensaje recibido del cliente: %s", message)
         store_secret_message(connection_socket, message)
 
     except Exception as e:
-        print("Error:", e)
+        logging.error("Error: %s", e)
 
     finally:
         # Cerrar la conexión
         connection_socket.close()
+        logging.info("Conexión cerrada con %s", address)
 
 def authenticate_user(connection_socket, username, password):
     # Verificar nombre de usuario y contraseña
@@ -65,7 +69,8 @@ def store_secret_message(connection_socket, message):
 
         # Enviar confirmación al cliente
         connection_socket.sendall(b"Mensaje almacenado correctamente.")
-        
+        logging.info("Mensaje", message, "almacenado correctamente")
+
     except Exception as e:
         print("Error al almacenar el mensaje:", e)
         connection_socket.sendall(b"Error al almacenar el mensaje.")
@@ -81,7 +86,7 @@ def main():
         # Escuchar conexiones entrantes
         server_socket.listen()
 
-        print(f"Servidor escuchando en {HOST}:{PORT}...")
+        logging.info(f"Servidor escuchando en {HOST}:{PORT}...")
 
         while True:
             # Aceptar conexiones entrantes
@@ -110,131 +115,13 @@ def main():
             client_thread.start()
 
     except Exception as e:
-        print("Error:", e)
+        logging.error("Error: %s", e)
 
     finally:
         # Cerrar el socket del servidor
+        logging.shutdown("Cerrando el servidor...")
         server_socket.close()
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-'''
-import json
-import socket
-import ssl
-import threading
-from ssl import PROTOCOL_TLS_SERVER
-
-HOST = ''
-PORT = 7070
-
-# Usuarios y contraseñas permitidos
-USERS = {
-    'usuario1': 'password1',
-    'usuario2': 'password2'
-}
-
-# Rutas a los archivos de clave privada y certificado del servidor
-KEYFILE = 'server-key.pem'
-CERTFILE = 'server-cert.pem'
-
-def handle_client(connection_socket, address):
-    try:
-        print("Conexión aceptada desde:", address)
-
-        data = connection_socket.recv(1024).decode()
-        client_data = json.loads(data)
-        username = client_data['username']
-        password = client_data['password']
-        message = client_data['message']
-
-        # Autenticar al usuario
-        auth_result = authenticate_user(connection_socket, username, password)
-        if not auth_result:
-            return
-        
-        print("Mensaje recibido del cliente:", message)
-        store_secret_message(connection_socket)
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        # Cerrar la conexión
-        connection_socket.close()
-
-def authenticate_user(connection_socket, username, password):
-    # Recibir nombre de usuario y contraseña del cliente
-
-    # Verificar nombre de usuario y contraseña
-    if username in USERS and USERS[username] == password:
-        return True
-    else:
-        connection_socket.sendall(b"Autenticacion fallida.")
-        return False
-
-def store_secret_message(connection_socket):
-    # Almacenar el mensaje secreto
-    connection_socket.sendall(b"Mensaje almacenado correctamente.")
-
-def main():
-    # Configuración del host y puerto del servidor
-
-
-    try:
-        # Crear un socket TCP/IP
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Enlazar el socket al host y puerto
-        server_socket.bind((HOST, PORT))
-
-        # Escuchar conexiones entrantes
-        server_socket.listen()
-
-        print(f"Servidor escuchando en {HOST}:{PORT}...")
-
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-
-        # Configurar la lista de Cipher Suites
-        cipher_suites = [
-                "TLS_AES_256_GCM_SHA384",
-                "TLS_AES_128_GCM_SHA256",
-                "TLS_AES_128_CCM_SHA256",
-                "TLS_AES_128_CCM_8_SHA256"
-        ]
-
-        # Configurar el contexto SSL/TLS
-        #ssl_context.set_ciphers(':'.join(cipher_suites))
-        ssl_context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
-
-        # Forzar el uso de TLS 1.3
-        ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
-        ssl_context.maximum_version = ssl.TLSVersion.TLSv1_3
-        
-        while True:
-            # Aceptar conexiones entrantes
-            connection_socket, addr = server_socket.accept()
-
-            # Utilizar SSL/TLS para el socket
-            ssl_socket = ssl_context.wrap_socket(connection_socket, server_side=True)
-
-            # Manejar el cliente en un hilo separado
-            client_thread = threading.Thread(target=handle_client, args=(ssl_socket, addr))
-            client_thread.start()
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        # Cerrar el socket del servidor
-        server_socket.close()
-
-if __name__ == "__main__":
-    main()
-
-'''
